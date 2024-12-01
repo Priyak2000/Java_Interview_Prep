7a5223895f012b4ef1ad806de5bde8e917d959d3
Priyak2000 <priyakpriya279@gmail.com>
2024-12-01 17:20:04 +0530

    SumArr file is added

---
+++ bin/arrayInterviewQstns/SumArr.java
@@ -0,0 +1,11 @@
+public class SumArr {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,5};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum=arr[i];
+        }
+        System.out.println("Sum of element : " + sum);
+    }
+    
+}
